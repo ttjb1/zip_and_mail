a5a8e1f9c5356876988695a419b8f0f13954f96a
Alexey Mkhaylov <amikhaylov@integrotechnologies.ru>
2023-04-01 20:53:02 +0300

    CLI + config

--- zip_files_CLI.py
+++ zip_files_CLI.py
@@ -8,6 +8,7 @@ from email.mime.text import MIMEText
 import smtplib
 import pathlib
 from datetime import datetime
+import json
 
 Dir = r"/Users/ttjb1/Downloads/python_project/demo_files/"
 Ext = r".txt"
@@ -79,42 +80,45 @@ def zip_dir_without_ext_absolute_path(Dir, OFile):
 #zip_dir_with_ext(Dir, Ext, OFile)
 #zip_dir_with_ext_absolute_path(Dir, Ext, OFile)
 #zip_dir_without_ext(Dir, OFile)
-zip_dir_without_ext_absolute_path(Dir, OFile)
+#zip_dir_without_ext_absolute_path(Dir, OFile)
 
-# def send_mail():
-#     # Create a multipart message
-#     msg = MIMEMultipart()
-#     body_part = MIMEText(MESSAGE_BODY, 'plain')
-#     msg['Subject'] = EMAIL_SUBJECT
-#     msg['From'] = EMAIL_FROM
-#     msg['To'] = EMAIL_TO
-#     # Add body to email
-#     msg.attach(body_part)
-#     # open and read the file in binary
-#     with open(PATH_TO_ZIP_FILE,'rb') as file:
-#     # Attach the file with filename to the email
-#         #msg.attach(MIMEApplication(file.read(), Name='filename.zip'))
-#         msg.attach(MIMEApplication(file.read(), Name = FILE_NAME))
+def send_mail():
+    # Create a multipart message
+    msg = MIMEMultipart()
+    body_part = MIMEText(MESSAGE_BODY, 'plain')
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = EMAIL_FROM
+    msg['To'] = EMAIL_TO
+    # Add body to email
+    msg.attach(body_part)
+    # open and read the file in binary
+    with open(PATH_TO_ZIP_FILE,'rb') as file:
+    # Attach the file with filename to the email
+        #msg.attach(MIMEApplication(file.read(), Name='filename.zip'))
+        msg.attach(MIMEApplication(file.read(), Name = FILE_NAME))
 
-#     # Create SMTP object
-#     smtp_obj = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
-#     # Login to the server
-#     smtp_obj.login(SMTP_USERNAME, SMTP_PASSWORD)
+    # Create SMTP object
+    smtp_obj = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+    # Login to the server
+    smtp_obj.login(SMTP_USERNAME, SMTP_PASSWORD)
 
-#     # Convert the message to a string and send it
-#     smtp_obj.sendmail(msg['From'], msg["To"].split(","), msg.as_string())
-#     smtp_obj.quit()
+    # Convert the message to a string and send it
+    smtp_obj.sendmail(msg['From'], msg["To"].split(","), msg.as_string())
+    smtp_obj.quit()
 
-# MESSAGE_BODY = "test_12"
-# EMAIL_SUBJECT = "test_subj_12"
-# EMAIL_FROM = "mr_meth@mail.ru"
-# EMAIL_TO = "turntablistjb@yandex.ru, alexei.mikhailov@gmail.com, amikhaylov@integrotechnologies.ru"
-# #PATH_TO_ZIP_FILE = r"/Users/ttjb1/Downloads/python_project/filename.zip"
-# #PATH_TO_ZIP_FILE = zip_dir(r"/Users/ttjb1/Downloads/python_project/demo_files/", r".txt", 'test_123_filename.zip')
-# PATH_TO_ZIP_FILE = zip_dir_with_ext(Dir, Ext, OFile)
-# FILE_NAME = OFile
-# SMTP_SERVER = 'smtp.mail.ru'
-# SMTP_PORT = 465
-# SMTP_USERNAME = 'mr_meth'
-# SMTP_PASSWORD = 'wSAynjEj7uGiY1uKkTzs'
-# send_mail()
+with open(r"/Users/ttjb1/Downloads/python_project/config_sample.json", 'r') as f:
+    config = json.load(f)
+MESSAGE_BODY = config['MESSAGE_BODY']
+EMAIL_SUBJECT = config['EMAIL_SUBJECT']
+EMAIL_FROM = config['EMAIL_FROM']
+EMAIL_TO = config['EMAIL_TO']
+SMTP_SERVER = config['SMTP_SERVER']
+SMTP_PORT = config['SMTP_PORT']
+SMTP_USERNAME = config['SMTP_USERNAME']
+SMTP_PASSWORD = config['SMTP_PASSWORD']
+
+
+PATH_TO_ZIP_FILE = zip_dir_with_ext(Dir, Ext, OFile)
+FILE_NAME = OFile
+
+send_mail()
